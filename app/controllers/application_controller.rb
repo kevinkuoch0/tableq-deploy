286fde67e252ffdd0da565b24183568850cd4544
Kevin Kuoch <kevinkuoch0@gmail.com>
2015-12-13 15:37:46 -0500

    redirects working and gutters added to navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,5 @@ class ApplicationController < ActionController::Base
 	def after_sign_in_path_for(owner)
 		signed_in_index_path
 	end
+
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
-  def index
-  end
+		def index
+			if user_signed_in?
+				redirect_to signed_in_index_path
+			elsif owner_signed_in?
+				redirect_to signed_in_index_path
+			else
+				root_path
+			end
+		end
 end
